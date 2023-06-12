d831638896b450f31a4a392534c41ee513a585b9
Rahul Kumar Pandit <55670332+rahulfrndz02@users.noreply.github.com>
2023-06-12 14:46:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	if(sampleObject.contains("rahul"))
+		return true;
+	else
+		return false;
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
+// const key = prompt("Enter Key.");
 alert(hasKey(key));
