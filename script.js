5790d14a391c3af1601d2b809bfeac08908f1632
Rahul Kumar Pandit <55670332+rahulfrndz02@users.noreply.github.com>
2023-06-14 16:14:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	if(sampleObject.contains(key))
-		return true;
-	else
+	for(let x in sampleObject){
+		if(x == key)
+		  return true;	
+	  else
 		return false;
 }
 
